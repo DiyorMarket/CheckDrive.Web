c994166ff3f493ea57e39f487ef48a7e915e0ccd
SharifovDeveloper <sharifovmuhammadali571@gmail.com>
2024-05-07 22:49:41 +0500

    Created DoctorReview class

---
+++ CheckDrive.Web/CheckDrive.Web/Models/DoctorReview.cs
@@ -0,0 +1,14 @@
+﻿namespace CheckDrive.Web.Models
+{
+    public class DoctorReview
+    {
+        public bool IsHealthy { get; set; }
+        public string? Comments { get; set; }
+        public DateTime Date { get; set; }
+
+        public int DriverId { get; set; }
+        public Driver? Driver { get; set; }
+        public int DoctorId { get; set; }
+        public Doctor? Doctor { get; set; }
+    }
+}
