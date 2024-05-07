b2daeaf0546b629169063f6e314f70207a37080c
SharifovDeveloper <sharifovmuhammadali571@gmail.com>
2024-05-07 22:50:49 +0500

    Created Mechanic class

---
+++ CheckDrive.Web/CheckDrive.Web/Models/Mechanic.cs
@@ -0,0 +1,12 @@
+﻿namespace CheckDrive.Web.Models
+{
+    public class Mechanic
+    {
+        public int Id { get; set; }
+        public int AccountId { get; set; }
+        public Account? Account { get; set; }
+
+        public ICollection<DispetcherReview>? DispetcherReviews { get; set; }
+        public ICollection<MechanicHandover>? MechanicHandovers { get; set; }
+    }
+}
