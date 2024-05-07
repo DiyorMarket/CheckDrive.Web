1dffce1d9aa28d59f4536e09714bffa33facf6dd
SharifovDeveloper <sharifovmuhammadali571@gmail.com>
2024-05-07 22:52:29 +0500

    Created Operator class

---
+++ CheckDrive.Web/CheckDrive.Web/Models/Operator.cs
@@ -0,0 +1,12 @@
+﻿namespace CheckDrive.Web.Models
+{
+    public class Operator
+    {
+        public int Id { get; set; }
+        public int AccountId { get; set; }
+        public Account? Account { get; set; }
+
+        public ICollection<DispetcherReview>? DispetcherReviews { get; set; }
+        public ICollection<OperatorReview>? OperatorReviews { get; set; }
+    }
+}
