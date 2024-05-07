85eeb0af2fe9405690adebaab8b7e990fa9bc960
SharifovDeveloper <sharifovmuhammadali571@gmail.com>
2024-05-07 22:46:01 +0500

    Created Car class

---
+++ CheckDrive.Web/CheckDrive.Web/Models/Car.cs
@@ -0,0 +1,15 @@
+﻿namespace CheckDrive.Web.Models
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        public string? Model { get; set; }
+        public string? Color { get; set; }
+        public string? Number { get; set; }
+        public double FuelConsumptionFor100Km { get; set; }
+        public double FuelTankCapacity { get; set; }
+        public int ManufacturedYear { get; set; }
+
+        public ICollection<MechanicHandover>? MechanicHandovers { get; set; }
+    }
+}
