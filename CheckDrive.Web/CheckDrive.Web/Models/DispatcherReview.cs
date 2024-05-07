3e80acf44b3a96397819555447bcc29862564afd
SharifovDeveloper <sharifovmuhammadali571@gmail.com>
2024-05-07 22:47:57 +0500

    Created DispatcherReview class

---
+++ CheckDrive.Web/CheckDrive.Web/Models/DispatcherReview.cs
@@ -0,0 +1,19 @@
+﻿namespace CheckDrive.Web.Models
+{
+    public class DispetcherReview
+    {
+        public int Id { get; set; }
+        public double FuelSpended { get; set; }
+        public double DistanceCovered { get; set; }
+        public DateTime Date { get; set; }
+
+        public int DispatcherId { get; set; }
+        public Dispatcher? Dispatcher { get; set; }
+        public int OperatorId { get; set; }
+        public Operator? Operator { get; set; }
+        public int MechanicId { get; set; }
+        public Mechanic? Mechanic { get; set; }
+        public int DriverId { get; set; }
+        public Driver? Driver { get; set; }
+    }
+}
