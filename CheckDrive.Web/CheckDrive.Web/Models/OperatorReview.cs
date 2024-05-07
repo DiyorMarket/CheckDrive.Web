175d82cc067ff57c6aa6c330e0a261a39e3f63ec
SharifovDeveloper <sharifovmuhammadali571@gmail.com>
2024-05-07 22:53:13 +0500

    Created OperatorReview class

---
+++ CheckDrive.Web/CheckDrive.Web/Models/OperatorReview.cs
@@ -0,0 +1,15 @@
+﻿namespace CheckDrive.Web.Models
+{
+    public class OperatorReview
+    {
+        public double OilAmount { get; set; }
+        public string? Comments { get; set; }
+        public Status Status { get; set; }
+        public DateTime Date { get; set; }
+
+        public int OperatorId { get; set; }
+        public Operator? Operator { get; set; }
+        public int DriverId { get; set; }
+        public Driver? Driver { get; set; }
+    }
+}
