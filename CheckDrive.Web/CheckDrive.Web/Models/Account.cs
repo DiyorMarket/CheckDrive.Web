5019340405171851778574acc9de6631a959dd24
SharifovDeveloper <sharifovmuhammadali571@gmail.com>
2024-05-07 22:44:54 +0500

    Created Account class

---
+++ CheckDrive.Web/CheckDrive.Web/Models/Account.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace CheckDrive.Web.Models
+{
+    public class Account
+    {
+        public int Id { get; set; }
+        public string? Login { get; set; }
+        public string? Password { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public DateTime Bithdate { get; set; }
+
+        public int RoleId { get; set; }
+        public Role? Role { get; set; }
+
+        public ICollection<Dispatcher>? Dispatchers { get; set; }
+        public ICollection<Operator>? Operators { get; set; }
+        public ICollection<Mechanic>? Mechanics { get; set; }
+        public ICollection<Driver>? Drivers { get; set; }
+        public ICollection<Doctor>? Doctors { get; set; }
+        public ICollection<Technician>? Technicians { get; set; }
+    }
+}
+
