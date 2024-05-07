4aafbf5b6400e6103df4bebc5ca9834ea7887f3c
SharifovDeveloper <sharifovmuhammadali571@gmail.com>
2024-05-07 22:53:50 +0500

    Created Role class

---
+++ CheckDrive.Web/CheckDrive.Web/Models/Role.cs
@@ -0,0 +1,10 @@
+﻿namespace CheckDrive.Web.Models
+{
+    public class Role
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+
+        public ICollection<Account> Accounts { get; set; }
+    }
+}
