53b788354bcf712dc98005b63fe3e9315400e9df
SharifovDeveloper <sharifovmuhammadali571@gmail.com>
2024-05-07 22:58:15 +0500

    Created Technician class

---
+++ CheckDrive.Web/CheckDrive.Web/Models/Technician.cs
@@ -0,0 +1,9 @@
+﻿namespace CheckDrive.Web.Models
+{
+    public class Technician
+    {
+        public int Id { get; set; }
+        public int AccountId { get; set; }
+        public Account? Account { get; set; }
+    }
+}
