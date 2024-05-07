d6090ab0f5ab24b02ba1a91e4953a1a88c60d281
SharifovDeveloper <sharifovmuhammadali571@gmail.com>
2024-05-07 22:50:09 +0500

    Created Driver class

---
+++ CheckDrive.Web/CheckDrive.Web/Models/Driver.cs
@@ -0,0 +1,14 @@
+﻿namespace CheckDrive.Web.Models
+{
+    public class Driver
+    {
+        public int Id { get; set; }
+        public int AccountId { get; set; }
+        public Account? Account { get; set; }
+
+        public ICollection<DispetcherReview>? DispetcherReviews { get; set; }
+        public ICollection<DoctorReview>? DoctorReviews { get; set; }
+        public ICollection<MechanicHandover>? MechanicHandovers { get; set; }
+        public ICollection<OperatorReview>? OperatorReviews { get; set; }
+    }
+}
