d5a4e33eb6a45b241dd6a3743809ea9f94536eb7
SharifovDeveloper <sharifovmuhammadali571@gmail.com>
2024-05-09 16:12:09 +0500

    Created IDispatcherReviewDataStore

---
+++ CheckDrive.Web/CheckDrive.Web/Stores/DispatcherReviews/IDispatcherReviewDataStore.cs
@@ -0,0 +1,13 @@
+﻿using CheckDrive.Web.Models;
+
+namespace CheckDrive.Web.Stores.DispatcherReviews
+{
+    public interface IDispatcherReviewDataStore
+    {
+        Task<List<DispatcherReview>> GetDispatcherReviews();
+        Task<DispatcherReview> GetDispatcherReview(int id);
+        Task<DispatcherReview> CreateDispatcherReview(DispatcherReview review);
+        Task<DispatcherReview> UpdateDispatcherReview(int id, DispatcherReview review);
+        Task DeleteDispatcherReview(int id);
+    }
+}
