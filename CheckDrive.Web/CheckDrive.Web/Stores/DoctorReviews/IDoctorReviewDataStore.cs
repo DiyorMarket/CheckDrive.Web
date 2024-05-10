1d60b73d5f438d2451bb2db8b4bb697939cc04db
SharifovDeveloper <sharifovmuhammadali571@gmail.com>
2024-05-10 11:15:24 +0500

    Created IDoctorReviewDataStore

---
+++ CheckDrive.Web/CheckDrive.Web/Stores/DoctorReviews/IDoctorReviewDataStore.cs
@@ -0,0 +1,13 @@
+﻿using CheckDrive.Web.Models;
+
+namespace CheckDrive.Web.Stores.DoctorReviews
+{
+    public interface IDoctorReviewDataStore
+    {
+        Task<List<DoctorReview>> GetDoctorReviews();
+        Task<DoctorReview> GetDoctorReview(int id);
+        Task<DoctorReview> CreateDoctorReview(DoctorReview review);
+        Task<DoctorReview> UpdateDoctorReview(int id, DoctorReview review);
+        Task DeleteDoctorReview(int id);
+    }
+}
