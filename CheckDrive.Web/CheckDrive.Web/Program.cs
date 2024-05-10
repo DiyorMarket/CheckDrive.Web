42d3a8478d14ab9f6b2b4509c170d1349d6d062a
AzamatG17 <128783946+AzamatG17@users.noreply.github.com>
2024-05-10 14:23:08 +0500

    Register Exception to StartUp

--- CheckDrive.Web/CheckDrive.Web/Program.cs
+++ CheckDrive.Web/CheckDrive.Web/Program.cs
@@ -1,7 +1,11 @@
+using CheckDrive.Web.Exceptions;
+using CheckDrive.Web.Filters;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllersWithViews();
+builder.Services.AddControllersWithViews(options =>
+    options.Filters.Add(new ApiExceptionFilter()));
 
 var app = builder.Build();
 
