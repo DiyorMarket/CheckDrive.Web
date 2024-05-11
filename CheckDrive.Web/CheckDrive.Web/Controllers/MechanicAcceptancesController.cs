e88f6b96902487e26682a61018d2b6f72c1303b8
SharifovDeveloper <sharifovmuhammadali571@gmail.com>
2024-05-11 14:44:07 +0500

    Created MechanicAcceptancesController

---
+++ CheckDrive.Web/CheckDrive.Web/Controllers/MechanicAcceptancesController.cs
@@ -0,0 +1,114 @@
+﻿using CheckDrive.Web.Models;
+using CheckDrive.Web.Stores.MechanicAcceptances;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CheckDrive.Web.Controllers
+{
+    public class MechanicAcceptancesController : Controller
+    {
+        private readonly IMechanicAcceptanceDataStore _mechanicAcceptanceDataStore;
+
+        public MechanicAcceptancesController(IMechanicAcceptanceDataStore mechanicAcceptanceDataStore)
+        {
+            _mechanicAcceptanceDataStore = mechanicAcceptanceDataStore;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var mechanicAcceptances = await _mechanicAcceptanceDataStore.GetMechanicAcceptances();
+            return View(mechanicAcceptances);
+        }
+
+        public async Task<IActionResult> Details(int id)
+        {
+            var mechanicAcceptance = await _mechanicAcceptanceDataStore.GetMechanicAcceptance(id);
+            if (mechanicAcceptance == null)
+            {
+                return NotFound();
+            }
+            return View(mechanicAcceptance);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("IsAccepted,Comments,Status,Date,Distance")] MechanicAcceptance mechanicAcceptance)
+        {
+            if (ModelState.IsValid)
+            {
+                await _mechanicAcceptanceDataStore.CreateMechanicAcceptance(mechanicAcceptance);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(mechanicAcceptance);
+        }
+
+        public async Task<IActionResult> Edit(int id)
+        {
+            var mechanicAcceptance = await _mechanicAcceptanceDataStore.GetMechanicAcceptance(id);
+            if (mechanicAcceptance == null)
+            {
+                return NotFound();
+            }
+            return View(mechanicAcceptance);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,IsAccepted,Comments,Status,Date,Distance")] MechanicAcceptance mechanicAcceptance)
+        {
+            if (id != mechanicAcceptance.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    await _mechanicAcceptanceDataStore.UpdateMechanicAcceptance(id, mechanicAcceptance);
+                }
+                catch (Exception)
+                {
+                    if (!await MechanicAcceptanceExists(id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(mechanicAcceptance);
+        }
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            var mechanicAcceptance = await _mechanicAcceptanceDataStore.GetMechanicAcceptance(id);
+            if (mechanicAcceptance == null)
+            {
+                return NotFound();
+            }
+            return View(mechanicAcceptance);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            await _mechanicAcceptanceDataStore.DeleteMechanicAcceptance(id);
+            return RedirectToAction(nameof(Index));
+        }
+
+        private async Task<bool> MechanicAcceptanceExists(int id)
+        {
+            var mechanicAcceptance = await _mechanicAcceptanceDataStore.GetMechanicAcceptance(id);
+            return mechanicAcceptance != null;
+        }
+    }
+}
