5f39d5d85da0a9bdb74f466b1b67b8702a88ef68
SharifovDeveloper <sharifovmuhammadali571@gmail.com>
2024-05-11 14:56:29 +0500

    Created OperatorReviewsController

---
+++ CheckDrive.Web/CheckDrive.Web/Controllers/OperatorReviewsController.cs
@@ -0,0 +1,114 @@
+﻿using CheckDrive.Web.Models;
+using CheckDrive.Web.Stores.OperatorReviews;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CheckDrive.Web.Controllers
+{
+    public class OperatorReviewsController : Controller
+    {
+        private readonly IOperatorReviewDataStore _operatorReviewDataStore;
+
+        public OperatorReviewsController(IOperatorReviewDataStore operatorReviewDataStore)
+        {
+            _operatorReviewDataStore = operatorReviewDataStore;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var operatorReviews = await _operatorReviewDataStore.GetOperatorReviews();
+            return View(operatorReviews);
+        }
+
+        public async Task<IActionResult> Details(int id)
+        {
+            var operatorReview = await _operatorReviewDataStore.GetOperatorReview(id);
+            if (operatorReview == null)
+            {
+                return NotFound();
+            }
+            return View(operatorReview);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("OilAmount,Comments,Status,Date,OperatorId,DriverId")] OperatorReview operatorReview)
+        {
+            if (ModelState.IsValid)
+            {
+                await _operatorReviewDataStore.CreateOperatorReview(operatorReview);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(operatorReview);
+        }
+
+        public async Task<IActionResult> Edit(int id)
+        {
+            var operatorReview = await _operatorReviewDataStore.GetOperatorReview(id);
+            if (operatorReview == null)
+            {
+                return NotFound();
+            }
+            return View(operatorReview);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,OilAmount,Comments,Status,Date,OperatorId,DriverId")] OperatorReview operatorReview)
+        {
+            if (id != operatorReview.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    await _operatorReviewDataStore.UpdateOperatorReview(id, operatorReview);
+                }
+                catch (Exception)
+                {
+                    if (!await OperatorReviewExists(id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(operatorReview);
+        }
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            var operatorReview = await _operatorReviewDataStore.GetOperatorReview(id);
+            if (operatorReview == null)
+            {
+                return NotFound();
+            }
+            return View(operatorReview);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            await _operatorReviewDataStore.DeleteOperatorReview(id);
+            return RedirectToAction(nameof(Index));
+        }
+
+        private async Task<bool> OperatorReviewExists(int id)
+        {
+            var operatorReview = await _operatorReviewDataStore.GetOperatorReview(id);
+            return operatorReview != null;
+        }
+    }
+}
