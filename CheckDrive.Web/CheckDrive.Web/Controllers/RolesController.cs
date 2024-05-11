0f1bb6bd0ec9c887ff357b2f5422d8c560b6b6b6
SharifovDeveloper <sharifovmuhammadali571@gmail.com>
2024-05-11 15:17:49 +0500

    Created TechniciansController

--- CheckDrive.Web/CheckDrive.Web/Controllers/RolesController.cs
+++ CheckDrive.Web/CheckDrive.Web/Controllers/RolesController.cs
@@ -1,9 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Threading.Tasks;
-using CheckDrive.Web.Models;
+﻿using CheckDrive.Web.Models;
 using CheckDrive.Web.Stores.Roles;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CheckDrive.Web.Controllers
 {
