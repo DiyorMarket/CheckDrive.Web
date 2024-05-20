954f90090a8411d482f7972de2b0c513f9ae3576
AzamatG17 <128783946+AzamatG17@users.noreply.github.com>
2024-05-20 16:52:52 +0500

    Create OperatorReview DataStore

--- CheckDrive.Web/CheckDrive.Web/Stores/OperatorReviews/IOperatorReviewDataStore.cs
+++ CheckDrive.Web/CheckDrive.Web/Stores/OperatorReviews/IOperatorReviewDataStore.cs
@@ -1,10 +1,11 @@
 ﻿using CheckDrive.Web.Models;
+using CheckDrive.Web.Responses;
 
 namespace CheckDrive.Web.Stores.OperatorReviews
 {
     public interface IOperatorReviewDataStore
     {
-        Task<List<OperatorReview>> GetOperatorReviews();
+        Task<GetOperatorReviewResponse> GetOperatorsReviews();
         Task<OperatorReview> GetOperatorReview(int id);
         Task<OperatorReview> CreateOperatorReview(OperatorReview operatorReview);
         Task<OperatorReview> UpdateOperatorReview(int id, OperatorReview operatorReview);
