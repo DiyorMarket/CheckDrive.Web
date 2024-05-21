652a42413598a08a37e4619f2b4903a95335daa0
SharifovDeveloper <sharifovmuhammadali571@gmail.com>
2024-05-21 22:50:36 +0500

    Corrected IMechanicAcceptanceDataStore

--- CheckDrive.Web/CheckDrive.Web/Stores/MechanicAcceptances/IMechanicAcceptanceDataStore.cs
+++ CheckDrive.Web/CheckDrive.Web/Stores/MechanicAcceptances/IMechanicAcceptanceDataStore.cs
@@ -5,7 +5,7 @@ namespace CheckDrive.Web.Stores.MechanicAcceptances
 {
     public interface IMechanicAcceptanceDataStore
     {
-        Task<GetMechanicResponse> GetMechanicAcceptancesAsync();
+        Task<GetMechanicAcceptanceResponse> GetMechanicAcceptancesAsync();
         Task<MechanicAcceptance> GetMechanicAcceptanceAsync(int id);
         Task<MechanicAcceptance> CreateMechanicAcceptanceAsync(MechanicAcceptance mechanicAcceptance);
         Task<MechanicAcceptance> UpdateMechanicAcceptanceAsync(int id, MechanicAcceptance mechanicAcceptance);
