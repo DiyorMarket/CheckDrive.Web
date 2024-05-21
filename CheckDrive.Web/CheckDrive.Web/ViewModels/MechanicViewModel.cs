912173402c86c97749b0a3bd6aadb8a86ce0eb90
SharifovDeveloper <sharifovmuhammadali571@gmail.com>
2024-05-21 12:11:53 +0500

    Created MechanicViewModel

---
+++ CheckDrive.Web/CheckDrive.Web/ViewModels/MechanicViewModel.cs
@@ -0,0 +1,12 @@
+﻿public class MechanicViewModel
+{
+    public int Id { get; set; }
+    public string MechanicName { get; set; }
+    public string DriverName { get; set; }
+    public string CarDetails { get; set; }
+    public double Distance { get; set; }
+    public bool IsAccepted { get; set; }
+    public string Comments { get; set; }
+    public string Status { get; set; }
+    public DateTime Date { get; set; }
+}
