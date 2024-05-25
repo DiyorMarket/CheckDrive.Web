233ea14bb4efe1b9a878ba99eff00ee6705a7cfe
SharifovDeveloper <sharifovmuhammadali571@gmail.com>
2024-05-25 15:05:41 +0500

    Updated HandoverResponse

--- CheckDrive.Web/CheckDrive.Web/Stores/MechanicHandovers/IMechanicHandoverDataStore.cs
+++ CheckDrive.Web/CheckDrive.Web/Stores/MechanicHandovers/IMechanicHandoverDataStore.cs
@@ -1,13 +1,14 @@
 ﻿using CheckDrive.Web.Models;
+using CheckDrive.Web.Responses;
 
 namespace CheckDrive.Web.Stores.MechanicHandovers
 {
     public interface IMechanicHandoverDataStore
     {
-        Task<List<MechanicHandover>> GetMechanicHandovers();
-        Task<MechanicHandover> GetMechanicHandover(int id);
-        Task<MechanicHandover> CreateMechanicHandover(MechanicHandover mechanicHandover);
-        Task<MechanicHandover> UpdateMechanicHandover(int id, MechanicHandover mechanicHandover);
-        Task DeleteMechanicHandover(int id);
+        Task<GetMechanicHandoverResponse> GetMechanicHandoversAsync();
+        Task<MechanicHandover> GetMechanicHandoverAsync(int id);
+        Task<MechanicHandover> CreateMechanicHandoverAsync(MechanicHandover mechanicHandover);
+        Task<MechanicHandover> UpdateMechanicHandoverasync(int id, MechanicHandover mechanicHandover);
+        Task DeleteMechanicHandoverAsync(int id);
     }
 }
