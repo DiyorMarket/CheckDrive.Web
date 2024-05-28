b02436704bef7b7cc1f6e4a51977e724221e3ab5
khojianvar <khojianvarabdumukhtorov@gmail.com>
2024-05-28 13:09:44 +0500

    Personal_Doctors_Page

--- CheckDrive.Web/CheckDrive.Web/Program.cs
+++ CheckDrive.Web/CheckDrive.Web/Program.cs
@@ -33,6 +33,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Dashboard}/{action=Index}/{id?}");
+    pattern: "{controller=Dashboard}/{action=index}/{id?}");
 
 app.Run();
