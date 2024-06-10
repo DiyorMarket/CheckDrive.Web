a1f3fedb8feae6dc6931a734abf8e21836165474
khojianvar <khojianvarabdumukhtorov@gmail.com>
2024-06-10 13:57:16 +0500

    car-property-fix

--- CheckDrive.Web/CheckDrive.Web/Controllers/MechanicAcceptancesController.cs
+++ CheckDrive.Web/CheckDrive.Web/Controllers/MechanicAcceptancesController.cs
@@ -62,10 +62,10 @@ namespace CheckDrive.Web.Controllers
             return View();
         }
 
-        public async Task<IActionResult> PersonalIndex(int? pageNumber)
+        public async Task<IActionResult> PersonalIndex(string? searchstring, int? pageNumber)
         {
             var drivers = await _driverDataStore.GetDriversAsync();
-            var cars = await _carDataStore.GetCarsAsync();
+            var cars = await _carDataStore.GetCarsAsync(searchstring, pageNumber);
             var mechanics = await _mechanicDataStore.GetMechanicsAsync();
             var response = await _mechanicAcceptanceDataStore.GetMechanicAcceptancesAsync(pageNumber);
 
@@ -246,7 +246,7 @@ namespace CheckDrive.Web.Controllers
 
         private async Task<List<SelectListItem>> GETCars()
         {
-            var carResponse = await _carDataStore.GetCarsAsync();
+            var carResponse = await _carDataStore.GetCarsAsync(null, null);
             var cars = carResponse.Data
                 .Select(c => new SelectListItem
                 {
