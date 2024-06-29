748b2a8f2205a32a480e28c87e8cc6421822c9db
Sharafiddin.Mukhtorov <129146459+SharafiddinMukhtorov@users.noreply.github.com>
2024-06-29 15:32:11 +0500

    Add links for LogOut dropdown and fixed Create buttuns

--- CheckDrive.Web/CheckDrive.Web/Service/ApiClient.cs
+++ CheckDrive.Web/CheckDrive.Web/Service/ApiClient.cs
@@ -6,7 +6,7 @@ namespace CheckDrive.Web.Service
 {
     public class ApiClient
     {
-        private const string baseUrl = "https://localhost:7111/api";
+        private const string baseUrl = "https://srvsrv10-7111.asse.devtunnels.ms/api";
 
         private readonly HttpClient _client = new();
         private readonly IHttpContextAccessor _contextAccessor;
