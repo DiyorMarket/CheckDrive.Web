ae4566c2d2b8e4e8ff271a3ad0e55df73c7d53ce
AzamatG17 <128783946+AzamatG17@users.noreply.github.com>
2024-08-06 12:34:36 +0500

    Create method for get excel file in datastore

--- CheckDrive.Web/CheckDrive.Web/Stores/MechanicHandovers/MechanicHandoverDataStore.cs
+++ CheckDrive.Web/CheckDrive.Web/Stores/MechanicHandovers/MechanicHandoverDataStore.cs
@@ -150,5 +150,38 @@ namespace CheckDrive.Web.Stores.MechanicHandovers
             }
         }
 
+        public async Task<Stream> GetExportFile(int year, int month)
+        {
+            try
+            {
+                string url = $"mechanics/handover/export?year={year}&month={month}";
+                var response = await _api.GetAsync(url);
+
+                if (!response.IsSuccessStatusCode)
+                {
+                    throw new Exception("Failed to retrieve the file.");
+                }
+
+                var stream = await response.Content.ReadAsStreamAsync();
+                if (stream == null || stream.Length == 0)
+                {
+                    Console.WriteLine("The file is empty or could not be retrieved.");
+                    return null; // or throw an exception based on your needs
+                }
+
+                return stream;
+            }
+            catch (InvalidOperationException ex)
+            {
+                // Log or handle the exception
+                Console.WriteLine($"An error occurred: {ex.Message}");
+                return null;
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception("", ex);
+            }
+        }
     }
 }
