3bb85297277a7bfb349fd13411324c0c24e4c162
Boburmirzo <137428656+Boburm1rzo@users.noreply.github.com>
2024-10-08 19:51:48 +0500

    dashboard controller changes

--- CheckDrive.Web/CheckDrive.Web/Controllers/DashboardController.cs
+++ CheckDrive.Web/CheckDrive.Web/Controllers/DashboardController.cs
@@ -2,9 +2,9 @@
 using CheckDrive.Web.Stores.Dashbord;
 using CheckDrive.Web.ViewModels;
 using Microsoft.AspNetCore.Mvc;
-using NuGet.Common;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace CheckDrive.Web.Controllers
 {
@@ -21,24 +21,29 @@ namespace CheckDrive.Web.Controllers
         public async Task<IActionResult> Index()
         {
             string token = HttpContext.Request.Cookies["tasty-cookies"];
+         
             var tokenHandler = new JwtSecurityTokenHandler();
             var jwtToken = tokenHandler.ReadToken(token) as JwtSecurityToken;
+            
             if (jwtToken == null)
             {
                 return RedirectToAction("Login", "Account");
             }
+            
             var accountId = jwtToken.Claims.First(claim => claim.Type == ClaimTypes.NameIdentifier).Value;
             int accountIds = Int32.Parse(accountId);
 
             TempData["UserName"] = _accountDataStore.GetAccountAsync(accountIds)
                 .Result.FirstName;
             TempData.Keep("UserName");
+            
             var dashboard = await _store.GetDashboard();
 
             if (dashboard is null)
             {
                 return BadRequest();
             }
+            
             SetViewBagProperties(dashboard);
 
             return View();
@@ -46,6 +51,7 @@ namespace CheckDrive.Web.Controllers
         private void SetViewBagProperties(DashboardViewModel dashboard)
         {
             var summary = dashboard.Summary;
+            var Debts = Newtonsoft.Json.JsonConvert.SerializeObject(dashboard.Debts);
 
             ViewBag.MonthlyFuelConsumption = summary.MonthlyFuelConsumption.ToString("0.00");
             ViewBag.CarsCount = summary.CarsCount;
@@ -53,6 +59,8 @@ namespace CheckDrive.Web.Controllers
 
             ViewBag.EmployeesCountByRole = dashboard.EmployeesCountByRoles;
             ViewBag.SplineChartData = dashboard.SplineCharts;
+            ViewBag.PetrolCount=dashboard.PetrolCounts;
+            ViewBag.Debts = Debts;
         }
     }
 }
