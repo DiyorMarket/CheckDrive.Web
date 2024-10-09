2f981c24ed8745f65ff985c6b3f890b0c9910b8d
SharifovDeveloper <sharifovmuhammadali571@gmail.com>
2024-10-09 14:42:38 +0500

    Create ApiResponseHandler

---
+++ CheckDrive.Web/CheckDrive.Web/Exceptions/ApiResponseHandler.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+
+namespace CheckDrive.Web.Exceptions;
+
+public static class ApiResponseHandler
+{
+    public static async Task<T> HandleApiResponse<T>(HttpResponseMessage response, string errorMessage)
+    {
+        if (!response.IsSuccessStatusCode)
+        {
+            throw new Exception(errorMessage);
+        }
+
+        var jsonResponse = await response.Content.ReadAsStringAsync();
+        return JsonConvert.DeserializeObject<T>(jsonResponse)
+               ?? throw new Exception("Failed to deserialize API response.");
+    }
+}
