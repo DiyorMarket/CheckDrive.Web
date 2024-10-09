8ef56654b897a1f2e4b81a3066f25faaa7c7d71e
SharifovDeveloper <sharifovmuhammadali571@gmail.com>
2024-10-09 14:10:47 +0500

    Create IDoctorDateStore

---
+++ CheckDrive.Web/CheckDrive.Web/Stores/Doctors/IDoctorDateStore.cs
@@ -0,0 +1,13 @@
+﻿using CheckDrive.ApiContracts.Doctor;
+using CheckDrive.Web.Responses;
+
+namespace CheckDrive.Web.Stores.Doctors
+{
+    public interface IDoctorDateStore
+    {
+        Task<GetDoctorResponse> GetDoctorsAsync(string? searchString, int? pageNumber);
+        Task<GetDoctorResponse> GetDoctorsAsync();
+        Task<DoctorDto> GetDoctorByIdAsync(int id);
+        Task<DoctorDto> CreateDoctorAsync(DoctorForCreateDto doctorForCreate);
+    }
+}
