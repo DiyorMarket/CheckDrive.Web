b7707b3457f467896244543c68aa084cb545a897
SharifovDeveloper <sharifovmuhammadali571@gmail.com>
2024-10-09 15:57:49 +0500

    Create IMechanicDataStore

---
+++ CheckDrive.Web/CheckDrive.Web/Stores/Mechanics/IMechanicDataStore.cs
@@ -0,0 +1,13 @@
+﻿using CheckDrive.DTOs.Mechanic;
+using CheckDrive.Web.Responses;
+
+namespace CheckDrive.Web.Stores.Mechanics
+{
+    public interface IMechanicDataStore
+    {
+        Task<GetMechanicResponse> GetMechanicsAsync(string? searchString, int? pageNumber);
+        Task<GetMechanicResponse> GetDoctorsAsync();
+        Task<MechanicDto> GetDoctorByIdAsync(int id);
+        Task<MechanicDto> CreateDoctorAsync(MechanicForCreateDto mechanicForCreate);
+    }
+}
