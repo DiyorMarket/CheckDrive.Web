81ef31cb5cd0be2a88774a2c464dad017d95e198
SharifovDeveloper <sharifovmuhammadali571@gmail.com>
2024-10-10 16:03:17 +0500

    Create MechanicHandoverDataStore

---
+++ CheckDrive.Web/CheckDrive.Web/Stores/MechanicHandovers/MechanicHandoverDataStore.cs
@@ -0,0 +1,58 @@
+﻿using CheckDrive.ApiContracts.MechanicHandover;
+using CheckDrive.Web.Exceptions;
+using CheckDrive.Web.Responses;
+using CheckDrive.Web.Service;
+using Newtonsoft.Json;
+using System.Web;
+
+namespace CheckDrive.Web.Stores.MechanicHandovers;
+
+public class MechanicHandoverDataStore : IMechanicHandoverDataStore
+{
+    private readonly ApiClient _api;
+
+    public MechanicHandoverDataStore(ApiClient apiClient)
+    {
+        _api = apiClient;
+    }
+
+    public async Task<GetMechanicHandoverResponse> GetMechanicHandoversAsync(string? searchString, int? pageNumber)
+    {
+        var query = HttpUtility.ParseQueryString(string.Empty);
+
+        if (!string.IsNullOrWhiteSpace(searchString))
+        {
+            query["searchString"] = searchString;
+        }
+        if (pageNumber != null)
+        {
+            query["pageNumber"] = pageNumber.ToString();
+        }
+
+        var response = await _api.GetAsync($"mechanics/handovers?{query}");
+
+        return await ApiResponseHandler.HandleApiResponse<GetMechanicHandoverResponse>(response, "Could not fetch handovers.");
+    }
+
+    public async Task<GetMechanicHandoverResponse> GetMechanicHandoversAsync()
+    {
+        var response = await _api.GetAsync("mechanic/handovers");
+
+        return await ApiResponseHandler.HandleApiResponse<GetMechanicHandoverResponse>(response, "Could not fetch handovers.");
+    }
+
+    public async Task<MechanicHandoverDto> GetMechanicHandoverByIdAsync(int id)
+    {
+        var response = await _api.GetAsync($"mechanics/handover/{id}");
+
+        return await ApiResponseHandler.HandleApiResponse<MechanicHandoverDto>(response, $"Could not fetch mechanic handover with id: {id}.");
+    }
+
+    public async Task<MechanicHandoverDto> CreateMechanicHandoverAsync(MechanicHandoverForCreateDto mechanicForCreate)
+    {
+        var json = JsonConvert.SerializeObject(mechanicForCreate);
+        var response = await _api.PostAsync("mechanics/handover", json);
+
+        return await ApiResponseHandler.HandleApiResponse<MechanicHandoverDto>(response, "Error creating mechanic handover.");
+    }
+}
