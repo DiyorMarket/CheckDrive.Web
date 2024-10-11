51eefb59616e528a19d843ef0899fa053244e088
Boburmirzo <137428656+Boburm1rzo@users.noreply.github.com>
2024-10-12 01:39:56 +0500

    ViewModels

---
+++ CheckDrive.Web/CheckDrive.Web/ViewModels/Dashboard/OilCount.cs
@@ -0,0 +1,13 @@
+﻿using CheckDrive.Web.Models.Enums;
+
+namespace CheckDrive.Web.ViewModels.Dashboard;
+
+public class OilCount
+{
+    public OilType Type { get; set; }
+    public string OilTypeText
+    {
+        get { return Type.ToString(); }
+    }
+    public int Count { get; set; }
+}
