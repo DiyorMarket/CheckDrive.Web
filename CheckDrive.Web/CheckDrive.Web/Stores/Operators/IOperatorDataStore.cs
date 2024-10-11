b2a2a3d5c12560c74433633b463dc6c8da1eae3d
SharifovDeveloper <sharifovmuhammadali571@gmail.com>
2024-10-11 19:02:33 +0500

    Create IOperatorDataStore

---
+++ CheckDrive.Web/CheckDrive.Web/Stores/Operators/IOperatorDataStore.cs
@@ -0,0 +1,14 @@
+﻿using CheckDrive.ApiContracts.Operator;
+using CheckDrive.Web.Responses;
+
+namespace CheckDrive.Web.Stores.Operators;
+
+public interface IOperatorDataStore
+{
+    Task<GetOperatorResponse> GetOperatorsAsync(string? searchString, int? pageNumber);
+    Task<GetOperatorResponse> GetOperatorsAsync();
+    Task<OperatorDto> GetOperatorByIdAsync(int id);
+    Task<OperatorDto> CreateOperatorAsync(OperatorForCreateDto operatorForCreate);
+}
+
+
