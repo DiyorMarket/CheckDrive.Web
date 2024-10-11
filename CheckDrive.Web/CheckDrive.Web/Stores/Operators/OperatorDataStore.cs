353e4da0b505cf83b45ec1ca09c5dee74c83ee14
SharifovDeveloper <sharifovmuhammadali571@gmail.com>
2024-10-11 19:06:44 +0500

    Create OperatorDataStore

---
+++ CheckDrive.Web/CheckDrive.Web/Stores/Operators/OperatorDataStore.cs
@@ -0,0 +1,58 @@
+﻿using CheckDrive.ApiContracts.Operator;
+using CheckDrive.Web.Exceptions;
+using CheckDrive.Web.Responses;
+using CheckDrive.Web.Service;
+using Newtonsoft.Json;
+using System.Web;
+
+namespace CheckDrive.Web.Stores.Operators;
+
+public class OperatorDataStore : IOperatorDataStore
+{
+    private readonly ApiClient _api;
+
+    public OperatorDataStore(ApiClient apiClient)
+    {
+        _api = apiClient;
+    }
+
+    public async Task<GetOperatorResponse> GetOperatorsAsync(string? searchString, int? pageNumber)
+    {
+        var query = HttpUtility.ParseQueryString(string.Empty);
+
+        if (!string.IsNullOrWhiteSpace(searchString))
+        {
+            query["searchString"] = searchString;
+        }
+        if (pageNumber != null)
+        {
+            query["pageNumber"] = pageNumber.ToString();
+        }
+
+        var response = await _api.GetAsync($"operators?{query}");
+
+        return await ApiResponseHandler.HandleApiResponse<GetOperatorResponse>(response, "Could not fetch operators.");
+    }
+
+    public async Task<GetOperatorResponse> GetOperatorsAsync()
+    {
+        var response = await _api.GetAsync("operators");
+
+        return await ApiResponseHandler.HandleApiResponse<GetOperatorResponse>(response, "Could not fetch operators.");
+    }
+
+    public async Task<OperatorDto> GetOperatorByIdAsync(int id)
+    {
+        var response = await _api.GetAsync($"operators/{id}");
+
+        return await ApiResponseHandler.HandleApiResponse<OperatorDto>(response, $"Could not fetch operator with id: {id}.");
+    }
+
+    public async Task<OperatorDto> CreateOperatorAsync(OperatorForCreateDto operatorForCreate)
+    {
+        var json = JsonConvert.SerializeObject(operatorForCreate);
+        var response = await _api.PostAsync("operators", json);
+
+        return await ApiResponseHandler.HandleApiResponse<OperatorDto>(response, "Error creating operator.");
+    }
+}
