e819c086be043a561a761a20be97f49d5bc397f0
SharifovDeveloper <sharifovmuhammadali571@gmail.com>
2024-10-11 19:10:19 +0500

    Create IMechanicAcceptanceDataStore

---
+++ CheckDrive.Web/CheckDrive.Web/Stores/MechanicAcceptances/IMechanicAcceptanceDataStore.cs
@@ -0,0 +1,13 @@
+﻿using CheckDrive.ApiContracts.MechanicAcceptance;
+using CheckDrive.Web.Responses;
+
+namespace CheckDrive.Web.Stores.MechanicAcceptances;
+
+public interface IMechanicAcceptanceDataStore
+{
+    Task<GetMechanicAcceptanceResponse> GetMechanicAcceptancesAsync(string? searchString, int? pageNumber);
+    Task<GetMechanicAcceptanceResponse> GetMechanicAcceptancesAsync();
+    Task<MechanicAcceptanceDto> GetMechanicAcceptanceByIdAsync(int id);
+    Task<MechanicAcceptanceDto> CreateMechanicAcceptanceAsync(MechanicAcceptanceForCreateDto acceptanceForCreate);
+}
+
