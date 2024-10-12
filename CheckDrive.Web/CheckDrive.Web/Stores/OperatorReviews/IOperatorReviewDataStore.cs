6279b0a2ac5a6e15801fdfd301f30fd0e0123d1c
SharifovDeveloper <sharifovmuhammadali571@gmail.com>
2024-10-12 23:10:24 +0500

    Create IOperatorReviewDataStore

---
+++ CheckDrive.Web/CheckDrive.Web/Stores/OperatorReviews/IOperatorReviewDataStore.cs
@@ -0,0 +1,12 @@
+﻿using CheckDrive.ApiContracts.OperatorReview;
+using CheckDrive.Web.Responses;
+
+namespace CheckDrive.Web.Stores.OperatorReviews;
+
+public interface IOperatorReviewDataStore
+{
+    Task<GetOperatorReviewResponse> GetOperatorReviewsAsync(string? searchString, int? pageNumber);
+    Task<GetOperatorReviewResponse> GetOperatorReviewsAsync();
+    Task<OperatorReviewDto> GetOperatorReviewByIdAsync(int id);
+    Task<OperatorReviewDto> CreateOperatorReviewAsync(OperatorReviewForCreateDto operatorForCreate);
+}
