24bf9230cb71c854e88bbba1e667830d6d8194da
SharifovDeveloper <sharifovmuhammadali571@gmail.com>
2024-10-12 23:29:44 +0500

    Create DoctorReviewDataStore

---
+++ CheckDrive.Web/CheckDrive.Web/Stores/DoctorReviews/DoctorReviewDataStore.cs
@@ -0,0 +1,59 @@
+﻿using CheckDrive.ApiContracts.DoctorReview;
+using CheckDrive.Web.Exceptions;
+using CheckDrive.Web.Responses;
+using CheckDrive.Web.Service;
+using Newtonsoft.Json;
+using System.Web;
+
+namespace CheckDrive.Web.Stores.DoctorReviews
+{
+    public class DoctorReviewDataStore : IDoctorReviewDataStore
+    {
+        private readonly ApiClient _api;
+
+        public DoctorReviewDataStore(ApiClient apiClient)
+        {
+            _api = apiClient;
+        }
+
+        public async Task<GetDoctorReviewResponse> GetDoctorReviewsAsync(string? searchString, int? pageNumber)
+        {
+            var query = HttpUtility.ParseQueryString(string.Empty);
+
+            if (!string.IsNullOrWhiteSpace(searchString))
+            {
+                query["searchString"] = searchString;
+            }
+            if (pageNumber != null)
+            {
+                query["pageNumber"] = pageNumber.ToString();
+            }
+
+            var response = await _api.GetAsync($"doctors/reviews?{query}");
+
+            return await ApiResponseHandler.HandleApiResponse<GetDoctorReviewResponse>(response, "Could not fetch doctor reviews.");
+        }
+
+        public async Task<GetDoctorReviewResponse> GetDoctorReviewsAsync()
+        {
+            var response = await _api.GetAsync("doctors/reviews");
+
+            return await ApiResponseHandler.HandleApiResponse<GetDoctorReviewResponse>(response, "Could not fetch doctor reviews.");
+        }
+
+        public async Task<DoctorReviewDto> GetDoctorByIdAsync(int id)
+        {
+            var response = await _api.GetAsync($"doctors/review/{id}");
+
+            return await ApiResponseHandler.HandleApiResponse<DoctorReviewDto>(response, $"Could not fetch doctor review with id: {id}.");
+        }
+
+        public async Task<DoctorReviewDto> CreateDoctorAsync(DoctorReviewForCreateDto doctorReviewForCreate)
+        {
+            var json = JsonConvert.SerializeObject(doctorReviewForCreate);
+            var response = await _api.PostAsync("doctors/review", json);
+
+            return await ApiResponseHandler.HandleApiResponse<DoctorReviewDto>(response, "Error creating doctor review.");
+        }
+    }
+}
