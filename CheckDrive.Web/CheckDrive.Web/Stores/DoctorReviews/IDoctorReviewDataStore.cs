c2debd1a8df547ac21744261cf86856234c950cc
SharifovDeveloper <sharifovmuhammadali571@gmail.com>
2024-10-12 23:20:15 +0500

    Create IDoctorReviewDataStore

---
+++ CheckDrive.Web/CheckDrive.Web/Stores/DoctorReviews/IDoctorReviewDataStore.cs
@@ -0,0 +1,12 @@
+﻿using CheckDrive.ApiContracts.DoctorReview;
+using CheckDrive.Web.Responses;
+
+namespace CheckDrive.Web.Stores.DoctorReviews;
+
+public interface IDoctorReviewDataStore
+{
+    Task<GetDoctorResponse> GetDoctorReviewsAsync(string? searchString, int? pageNumber);
+    Task<GetDoctorResponse> GetDoctorReviewsAsync();
+    Task<DoctorReviewDto> GetDoctorReviewByIdAsync(int id);
+    Task<DoctorReviewDto> CreateDoctorReviewAsync(DoctorReviewForCreateDto doctorReviewForCreate);
+}
