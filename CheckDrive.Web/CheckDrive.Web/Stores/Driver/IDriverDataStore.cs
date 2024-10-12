797bfedf626bb28de33238047b30eaaef3aafa2a
SharifovDeveloper <sharifovmuhammadali571@gmail.com>
2024-10-12 10:16:18 +0500

    Create IDriverDataStore

---
+++ CheckDrive.Web/CheckDrive.Web/Stores/Driver/IDriverDataStore.cs
@@ -0,0 +1,13 @@
+﻿using CheckDrive.ApiContracts.Driver;
+using CheckDrive.Web.Responses;
+
+namespace CheckDrive.Web.Stores.Driver;
+
+public interface IDriverDataStore
+{
+    Task<GetDriverResponse> GetDriversAsync(string? searchString, int? pageNumber);
+    Task<GetDriverResponse> GetDriversAsync();
+    Task<DriverDto> GetDriverByIdAsync(int id);
+    Task<DriverDto> CreateDriverAsync(DriverForCreateDto driverForCreate);
+}
+
