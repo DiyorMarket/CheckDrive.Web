61f9782ee176a08b0969c982ba62b7ccf9dc3215
SharifovDeveloper <sharifovmuhammadali571@gmail.com>
2024-10-12 10:40:05 +0500

    Create IDispatcherReviewDataStore

---
+++ CheckDrive.Web/CheckDrive.Web/Stores/DispatcherReviewDataStore/IDispatcherReviewDataStore.cs
@@ -0,0 +1,12 @@
+﻿using CheckDrive.ApiContracts.DispatcherReview;
+using CheckDrive.Web.Responses;
+
+namespace CheckDrive.Web.Stores.DispatcherReviewDataStore;
+
+public interface IDispatcherReviewDataStore
+{
+    Task<GetDispatcherReviewResponse> GetDispatcherReviewsAsync(string? searchString, int? pageNumber);
+    Task<GetDispatcherReviewResponse>GetDispatcherReviewsAsync();
+    Task<DispatcherReviewDto> GetDispatcherReviewByIdAsync(int id);
+    Task<DispatcherReviewDto> CreateDispatcherReviewAsync(DispatcherReviewForCreateDto dispatcherForCreate);
+}
