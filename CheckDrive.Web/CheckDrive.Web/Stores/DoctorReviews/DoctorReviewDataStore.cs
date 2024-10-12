b415aa34469d7aef9977e6eb4afb6e9c96ddb55d
SharifovDeveloper <sharifovmuhammadali571@gmail.com>
2024-10-12 23:31:31 +0500

    Update DoctorReviewDataStore

--- CheckDrive.Web/CheckDrive.Web/Stores/DoctorReviews/DoctorReviewDataStore.cs
+++ CheckDrive.Web/CheckDrive.Web/Stores/DoctorReviews/DoctorReviewDataStore.cs
@@ -5,55 +5,54 @@ using CheckDrive.Web.Service;
 using Newtonsoft.Json;
 using System.Web;
 
-namespace CheckDrive.Web.Stores.DoctorReviews
+namespace CheckDrive.Web.Stores.DoctorReviews;
+
+public class DoctorReviewDataStore : IDoctorReviewDataStore
 {
-    public class DoctorReviewDataStore : IDoctorReviewDataStore
+    private readonly ApiClient _api;
+
+    public DoctorReviewDataStore(ApiClient apiClient)
     {
-        private readonly ApiClient _api;
+        _api = apiClient;
+    }
 
-        public DoctorReviewDataStore(ApiClient apiClient)
+    public async Task<GetDoctorReviewResponse> GetDoctorReviewsAsync(string? searchString, int? pageNumber)
+    {
+        var query = HttpUtility.ParseQueryString(string.Empty);
+
+        if (!string.IsNullOrWhiteSpace(searchString))
         {
-            _api = apiClient;
+            query["searchString"] = searchString;
+        }
+        if (pageNumber != null)
+        {
+            query["pageNumber"] = pageNumber.ToString();
         }
 
-        public async Task<GetDoctorReviewResponse> GetDoctorReviewsAsync(string? searchString, int? pageNumber)
-        {
-            var query = HttpUtility.ParseQueryString(string.Empty);
+        var response = await _api.GetAsync($"doctors/reviews?{query}");
 
-            if (!string.IsNullOrWhiteSpace(searchString))
-            {
-                query["searchString"] = searchString;
-            }
-            if (pageNumber != null)
-            {
-                query["pageNumber"] = pageNumber.ToString();
-            }
+        return await ApiResponseHandler.HandleApiResponse<GetDoctorReviewResponse>(response, "Could not fetch doctor reviews.");
+    }
 
-            var response = await _api.GetAsync($"doctors/reviews?{query}");
+    public async Task<GetDoctorReviewResponse> GetDoctorReviewsAsync()
+    {
+        var response = await _api.GetAsync("doctors/reviews");
 
-            return await ApiResponseHandler.HandleApiResponse<GetDoctorReviewResponse>(response, "Could not fetch doctor reviews.");
-        }
+        return await ApiResponseHandler.HandleApiResponse<GetDoctorReviewResponse>(response, "Could not fetch doctor reviews.");
+    }
 
-        public async Task<GetDoctorReviewResponse> GetDoctorReviewsAsync()
-        {
-            var response = await _api.GetAsync("doctors/reviews");
+    public async Task<DoctorReviewDto> GetDoctorReviewByIdAsync(int id)
+    {
+        var response = await _api.GetAsync($"doctors/review/{id}");
 
-            return await ApiResponseHandler.HandleApiResponse<GetDoctorReviewResponse>(response, "Could not fetch doctor reviews.");
-        }
+        return await ApiResponseHandler.HandleApiResponse<DoctorReviewDto>(response, $"Could not fetch doctor review with id: {id}.");
+    }
 
-        public async Task<DoctorReviewDto> GetDoctorByIdAsync(int id)
-        {
-            var response = await _api.GetAsync($"doctors/review/{id}");
+    public async Task<DoctorReviewDto> CreateDoctorReviewAsync(DoctorReviewForCreateDto doctorReviewForCreate)
+    {
+        var json = JsonConvert.SerializeObject(doctorReviewForCreate);
+        var response = await _api.PostAsync("doctors/review", json);
 
-            return await ApiResponseHandler.HandleApiResponse<DoctorReviewDto>(response, $"Could not fetch doctor review with id: {id}.");
-        }
-
-        public async Task<DoctorReviewDto> CreateDoctorAsync(DoctorReviewForCreateDto doctorReviewForCreate)
-        {
-            var json = JsonConvert.SerializeObject(doctorReviewForCreate);
-            var response = await _api.PostAsync("doctors/review", json);
-
-            return await ApiResponseHandler.HandleApiResponse<DoctorReviewDto>(response, "Error creating doctor review.");
-        }
+        return await ApiResponseHandler.HandleApiResponse<DoctorReviewDto>(response, "Error creating doctor review.");
     }
 }
