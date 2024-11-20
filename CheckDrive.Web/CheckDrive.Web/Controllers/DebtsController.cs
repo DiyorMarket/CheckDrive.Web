101a1856ae09e06a4299a85bfd90ee5e94afbea5
Boburmirzo <137428656+Boburm1rzo@users.noreply.github.com>
2024-11-20 22:19:57 +0500

    Debts Controller fixed bugs

--- CheckDrive.Web/CheckDrive.Web/Controllers/DebtsController.cs
+++ CheckDrive.Web/CheckDrive.Web/Controllers/DebtsController.cs
@@ -1,23 +1,22 @@
 ﻿using CheckDrive.Web.Stores.Debts;
 using Microsoft.AspNetCore.Mvc;
 
-namespace CheckDrive.Web.Controllers
-{
-    public class DebtsController : Controller
-    {
-        private readonly IDebtsStore _store;
-        public DebtsController(IDebtsStore debtsStore)
-        {
-            _store = debtsStore;
-        }
-        public ActionResult Index(string? searchText, string? status)
-        {
-            var debts = _store.GetAll(searchText,status);
+namespace CheckDrive.Web.Controllers;
 
-            ViewBag.Status = _store.GetEnum();
-            ViewBag.SearchText = searchText;
-            ViewBag.SelectedStatus = status;
-            return View(debts);
-        }
+public class DebtsController : Controller
+{
+    private readonly IDebtsStore _store;
+    public DebtsController(IDebtsStore debtsStore)
+    {
+        _store = debtsStore ?? throw new ArgumentNullException(nameof(debtsStore));
+    }
+    public ActionResult Index(string? searchText, string? status)
+    {
+        var debts = _store.GetAll(searchText,status);
+
+        ViewBag.Status = _store.GetEnum();
+        ViewBag.SearchText = searchText;
+        ViewBag.SelectedStatus = status;
+        return View(debts);
     }
 }
