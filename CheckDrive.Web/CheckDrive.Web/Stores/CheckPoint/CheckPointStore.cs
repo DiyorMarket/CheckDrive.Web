2f3f30ee9062d84c1ed0ec0f9fcd735d5c869e66
aXayitov <hayitovabdurahmon76@gmail.com>
2024-12-05 18:31:59 +0500

    Added CheckPointDetails page

--- CheckDrive.Web/CheckDrive.Web/Stores/CheckPoint/CheckPointStore.cs
+++ CheckDrive.Web/CheckDrive.Web/Stores/CheckPoint/CheckPointStore.cs
@@ -15,165 +15,135 @@ public class CheckPointStore : ICheckPointStore
                 Id = 1,
                 StartDate = "22.01.2024",
                 Driver = "Ali",
-                Doctor = "Sardor Mamurov",
                 Mechanic = "Jahongir",
                 Operator = "Ronaldo",
-                Dispatcher = "Lenoks",
-                Status = DebtStatus.Paid
+                Dispatcher = "Lenoks"
             },
             new CheckPointViewModel
             {
                 Id = 2,
                 StartDate = "23.01.2024",
                 Driver = "Vali",
-                Doctor = "Shavkat Madaminov",
                 Mechanic = "Bahodir",
                 Operator = "Messi",
-                Dispatcher = "Diyor",
-                Status = DebtStatus.Unpaid
+                Dispatcher = "Diyor"
             },
             new CheckPointViewModel
             {
                 Id = 3,
                 StartDate = "24.01.2024",
                 Driver = "Hasan",
-                Doctor = "Ibrohim Turgunov",
                 Mechanic = "Shavkat",
                 Operator = "Mbappe",
-                Dispatcher = "Bobur",
-                Status = DebtStatus.Paid
+                Dispatcher = "Bobur"
             },
             new CheckPointViewModel
             {
                 Id = 4,
                 StartDate = "25.01.2024",
                 Driver = "Husan",
-                Doctor = "Diyor Rahmonov",
                 Mechanic = "Azamat",
                 Operator = "Xavi",
                 Dispatcher = "Ulug'bek",
-                Status = DebtStatus.Unpaid
             },
             new CheckPointViewModel
             {
                 Id = 5,
                 StartDate = "26.01.2024",
                 Driver = "Bekzod",
-                Doctor = "Rustam Soliyev",
                 Mechanic = "Anvar",
                 Operator = "Iniesta",
                 Dispatcher = "Javohir",
-                Status = DebtStatus.Paid
             },
             new CheckPointViewModel
             {
                 Id = 6,
                 StartDate = "27.01.2024",
                 Driver = "Alisher",
-                Doctor = "Komil Xolmatov",
                 Mechanic = "Dilshod",
                 Operator = "Benzema",
                 Dispatcher = "Olimjon",
-                Status = DebtStatus.Unpaid
             },
             new CheckPointViewModel
             {
                 Id = 7,
                 StartDate = "28.01.2024",
                 Driver = "Jamshid",
-                Doctor = "Akmal Karimov",
                 Mechanic = "Shohruh",
                 Operator = "Haaland",
                 Dispatcher = "Temur",
-                Status = DebtStatus.Paid
             },
             new CheckPointViewModel
             {
                 Id = 8,
                 StartDate = "29.01.2024",
                 Driver = "Mirzo",
-                Doctor = "Tursun Qobilov",
                 Mechanic = "Sherzod",
                 Operator = "Salah",
                 Dispatcher = "Ilyos",
-                Status = DebtStatus.Unpaid
             },
             new CheckPointViewModel
             {
                 Id = 9,
                 StartDate = "30.01.2024",
                 Driver = "Olim",
-                Doctor = "Akbar Norboev",
                 Mechanic = "Jasur",
                 Operator = "Kane",
-                Dispatcher = "Ravshan",
-                Status = DebtStatus.Paid
+                Dispatcher = "Ravshan"
             },
             new CheckPointViewModel
             {
                 Id = 10,
                 StartDate = "31.01.2024",
                 Driver = "Rustam",
-                Doctor = "Sanjar Tursunov",
                 Mechanic = "Farruh",
                 Operator = "Modric",
-                Dispatcher = "Shodmon",
-                Status = DebtStatus.Unpaid
+                Dispatcher = "Shodmon"
             },
             new CheckPointViewModel
             {
                 Id = 11,
                 StartDate = "01.02.2024",
                 Driver = "Sardor",
-                Doctor = "Bekmurod Shamsiyev",
                 Mechanic = "Eldor",
                 Operator = "Griezmann",
-                Dispatcher = "Farhod",
-                Status = DebtStatus.Paid
+                Dispatcher = "Farhod"
             },
             new CheckPointViewModel
             {
                 Id = 12,
                 StartDate = "02.02.2024",
                 Driver = "Farrux",
-                Doctor = "Sohib Qurbonov",
                 Mechanic = "Alisher",
                 Operator = "Suarez",
-                Dispatcher = "Iskandar",
-                Status = DebtStatus.Unpaid
+                Dispatcher = "Iskandar"
             },
             new CheckPointViewModel
             {
                 Id = 13,
                 StartDate = "03.02.2024",
                 Driver = "Jahongir",
-                Doctor = "Yusuf Mamatov",
                 Mechanic = "Rustam",
                 Operator = "Dybala",
-                Dispatcher = "Komron",
-                Status = DebtStatus.Paid
+                Dispatcher = "Komron"
             },
             new CheckPointViewModel
             {
                 Id = 14,
                 StartDate = "04.02.2024",
                 Driver = "Zafar",
-                Doctor = "Qodir Salomov",
                 Mechanic = "Nodir",
                 Operator = "Zidane",
-                Dispatcher = "Sardor",
-                Status = DebtStatus.Unpaid
+                Dispatcher = "Sardor"
             },
             new CheckPointViewModel
             {
                 Id = 15,
                 StartDate = "05.02.2024",
                 Driver = "Aziz",
-                Doctor = "Anvar Bekmurodov",
                 Mechanic = "Akobir",
                 Operator = "Ramos",
-                Dispatcher = "Dilshod",
-                Status = DebtStatus.Paid
+                Dispatcher = "Dilshod"
             }
         };
 
@@ -181,8 +151,7 @@ public class CheckPointStore : ICheckPointStore
 
         if (!string.IsNullOrEmpty(search))
         {
-            result.AddRange(checkPoints.Where(cp => cp.Doctor.Contains(search) ||
-                            cp.Driver.Contains(search) ||
+            result.AddRange(checkPoints.Where(cp => cp.Driver.Contains(search) ||
                             cp.Dispatcher.Contains(search) ||
                             cp.Mechanic.Contains(search) ||
                             cp.Operator.Contains(search)));
--- CheckDrive.Web/CheckDrive.Web/ViewModels/CheckPoint/CheckPointViewModel.cs
+++ CheckDrive.Web/CheckDrive.Web/ViewModels/CheckPoint/CheckPointViewModel.cs
@@ -7,11 +7,18 @@ namespace CheckDrive.Web.ViewModels.CheckPoint
         public int Id { get; set; }
         public string StartDate { get; set; }
         public string Driver { get; set; }
-        public string Doctor { get; set; }
+        public string CarModel { get; set; }
+        public decimal CurrentMileage { get; set; }
+        public decimal CurrentFuelAmount { get; set; }
         public string Mechanic { get; set; }
+        public decimal InitialMillage { get; set; }
+        public decimal FinalMileage { get; set; }
         public string Operator { get; set; }
+        public decimal InitialOilAmount { get; set; }
+        public decimal OilRefillAmount { get; set; }
+        public string Oil { get; set; }
         public string Dispatcher { get; set; }
-        public DebtStatus Status { get; set; }
-
+        public decimal FuelConsumptionAdjustment { get; set; }
+        public decimal DebtAmount { get; set; }
     }
 }
