e2e16bdfcdcb28762ac1f6fbdfb7752b365adef9
Miraziz.Khidoyatov <khidoyatov1997@gmail.com>
2024-12-31 20:08:47 +0500

    refactor exception filter

--- CheckDrive.Web/CheckDrive.Web/Filters/ExceptionFilter.cs
+++ CheckDrive.Web/CheckDrive.Web/Filters/ExceptionFilter.cs
@@ -1,6 +1,6 @@
-﻿using CheckDrive.Web.Exceptions;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.IdentityModel.Tokens;
 
 namespace CheckDrive.Web.Filters;
 
@@ -15,27 +15,23 @@ public class ExceptionFilter : IExceptionFilter
 
     public void OnException(ExceptionContext context)
     {
-        var ex = context.Exception;
+        var exception = context.Exception;
+
         _logger.LogError(
             context.Exception,
             "Unhandled HTTP exception occurred, {@Message}",
-            ex.Message);
+            exception.Message);
 
-        if (context.Exception is ApiException exception)
-        {
-            int statusCode = (int)exception.StatusCode;
-            context.Result = new RedirectToActionResult("ErrorPage", "Error", new { statusCode });
-            context.ExceptionHandled = true;
-        }
-        else if (context.Exception is HttpRequestException error)
-        {
-            context.Result = new RedirectToActionResult("ErrorPage", "Error", new { error.StatusCode });
-            context.ExceptionHandled = true;
-        }
-        else
-        {
-            context.Result = new RedirectToActionResult("ErrorPage", "Error", new { statusCode = 500 });
-            context.ExceptionHandled = true;
-        }
+        var statusCode = GetStatusCode(exception);
+        context.Result = new RedirectToActionResult("ErrorPage", "Error", new { statusCode });
+        context.ExceptionHandled = true;
     }
+
+    private static int GetStatusCode(Exception ex) =>
+        ex switch
+        {
+            HttpRequestException apiException => apiException.StatusCode.HasValue ? (int)apiException.StatusCode.Value : 500,
+            SecurityTokenException => StatusCodes.Status401Unauthorized,
+            _ => 500
+        };
 }
