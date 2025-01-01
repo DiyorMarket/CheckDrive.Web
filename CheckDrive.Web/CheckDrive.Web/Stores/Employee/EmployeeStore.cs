070c1a87288172fb6097fb4788d67267f224eba2
Miraziz.Khidoyatov <khidoyatov1997@gmail.com>
2025-01-01 23:52:43 +0500

    implement employee store

---
+++ CheckDrive.Web/CheckDrive.Web/Stores/Employee/EmployeeStore.cs
@@ -0,0 +1,31 @@
+﻿using CheckDrive.Web.Requests.Employee;
+using CheckDrive.Web.Services;
+using CheckDrive.Web.ViewModels.Employee;
+
+namespace CheckDrive.Web.Stores.Employee;
+
+internal sealed class EmployeeStore : IEmployeeStore
+{
+    private static readonly string resourceUrl = "employees";
+    private readonly CheckDriveApi _apiClient;
+
+    public EmployeeStore(CheckDriveApi apiClient)
+    {
+        _apiClient = apiClient ?? throw new ArgumentNullException(nameof(apiClient));
+    }
+
+    public Task<List<EmployeeViewModel>> GetAsync()
+        => _apiClient.GetAsync<List<EmployeeViewModel>>(resourceUrl);
+
+    public Task<EmployeeViewModel> GetByIdAsync(int id)
+        => _apiClient.GetAsync<EmployeeViewModel>($"{resourceUrl}/{id}");
+
+    public Task<EmployeeViewModel> CreateAsync(CreateEmployeeRequest request)
+        => _apiClient.PostAsync<CreateEmployeeRequest, EmployeeViewModel>(resourceUrl, request);
+
+    public Task UpdateAsync(UpdateEmployeeRequest request)
+        => _apiClient.PutAsync($"{resourceUrl}/{request.Id}", request);
+
+    public Task DeleteAsync(int id)
+        => _apiClient.DeleteAsync($"{resourceUrl}/{id}");
+}
---
+++ CheckDrive.Web/CheckDrive.Web/Stores/Employee/IEmployeeStore.cs
@@ -0,0 +1,13 @@
+﻿using CheckDrive.Web.Requests.Employee;
+using CheckDrive.Web.ViewModels.Employee;
+
+namespace CheckDrive.Web.Stores.Employee;
+
+public interface IEmployeeStore
+{
+    Task<List<EmployeeViewModel>> GetAsync();
+    Task<EmployeeViewModel> GetByIdAsync(int id);
+    Task<EmployeeViewModel> CreateAsync(CreateEmployeeRequest request);
+    Task UpdateAsync(UpdateEmployeeRequest request);
+    Task DeleteAsync(int id);
+}
