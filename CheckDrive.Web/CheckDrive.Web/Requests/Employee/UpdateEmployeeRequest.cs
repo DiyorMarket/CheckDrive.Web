2c2942c09f5e17ecae63c26166d92eda11737db6
Miraziz.Khidoyatov <khidoyatov1997@gmail.com>
2025-01-01 23:51:46 +0500

    implement employee views

---
+++ CheckDrive.Web/CheckDrive.Web/Requests/Employee/UpdateEmployeeRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace CheckDrive.Web.Requests.Employee;
+
+public class UpdateEmployeeRequest : CreateEmployeeRequest
+{
+    public int Id { get; set; }
+    public required string AccountId { get; set; }
+}
