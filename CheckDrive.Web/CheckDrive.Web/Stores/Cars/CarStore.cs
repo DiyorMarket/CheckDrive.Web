83104a307405ab986157cc9e4a9047c1f9dd4a0c
Ramadan1011 <shukhratovich75@gmail.com>
2025-01-30 16:17:22 +0500

    stores updated and renamed

---
+++ CheckDrive.Web/CheckDrive.Web/Stores/Cars/CarStore.cs
@@ -0,0 +1,25 @@
+﻿using CheckDrive.Web.Requests.Cars;
+using CheckDrive.Web.Services;
+using CheckDrive.Web.ViewModels.Car;
+
+namespace CheckDrive.Web.Stores.Cars;
+
+internal sealed class CarStore(CheckDriveApi apiClient) : ICarStore
+{
+    private static readonly string resourceUrl = "cars";
+
+    public Task<List<CarViewModel>> GetAsync()
+        => apiClient.GetAsync<List<CarViewModel>>(resourceUrl);
+
+    public Task<CarViewModel> GetByIdAsync(int id)
+        => apiClient.GetAsync<CarViewModel>($"{resourceUrl}/{id}");
+
+    public Task<CarViewModel> CreateAsync(CreateCarRequest request)
+        => apiClient.PostAsync<CreateCarRequest, CarViewModel>(resourceUrl, request);
+
+    public Task UpdateAsync(UpdateCarRequest request)
+        => apiClient.PutAsync($"{resourceUrl}/{request.Id}", request);
+
+    public Task DeleteAsync(int id)
+        => apiClient.DeleteAsync($"{resourceUrl}/{id}");
+}
---
+++ CheckDrive.Web/CheckDrive.Web/Stores/OilMarks/OilMarkStore.cs
@@ -0,0 +1,25 @@
+﻿using CheckDrive.Web.Requests.OilMark;
+using CheckDrive.Web.Services;
+using CheckDrive.Web.ViewModels.OilMark;
+
+namespace CheckDrive.Web.Stores.OilMarks;
+
+public class OilMarkStore(CheckDriveApi apiClient) : IOilMarkStore
+{
+    private static readonly string resourceUrl = "oilMarks";
+    
+    public Task<List<OilMarkViewModel>> GetAsync()
+        => apiClient.GetAsync<List<OilMarkViewModel>>(resourceUrl);
+
+    public Task<OilMarkViewModel> GetByIdAsync(int id)
+        => apiClient.GetAsync<OilMarkViewModel>($"{resourceUrl}/{id}");
+
+    public Task<OilMarkViewModel> CreateAsync(CreateOilMarkRequest request)
+        => apiClient.PostAsync<CreateOilMarkRequest, OilMarkViewModel>(resourceUrl, request);
+
+    public Task UpdateAsync(UpdateOilMarkRequest request)
+        => apiClient.PutAsync($"{resourceUrl}/{request.Id}", request);
+
+    public Task DeleteAsync(int id)
+        => apiClient.DeleteAsync($"{resourceUrl}/{id}");
+}
