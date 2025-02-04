66e830baf09b59fe0ff87ca48f34599848f0fe08
Ramadan1011 <shukhratovich75@gmail.com>
2025-02-04 16:27:17 +0500

    created oil-mark-mappings and used in controller

---
+++ CheckDrive.Web/CheckDrive.Web/Mappings/OilMarkMappings.cs
@@ -0,0 +1,14 @@
+﻿using CheckDrive.Web.Requests.OilMark;
+using CheckDrive.Web.ViewModels.OilMark;
+
+namespace CheckDrive.Web.Mappings;
+
+public static class OilMarkMappings
+{
+    public static UpdateOilMarkRequest ToUpdateViewModel(this OilMarkViewModel oilMark) =>
+        new()
+        {
+            Id = oilMark.Id,
+            Name = oilMark.Name
+        };
+}
