838da142ec0c4cedb8dc2f9ab9e826af3cf6cee8
Boburmirzo <137428656+Boburm1rzo@users.noreply.github.com>
2025-02-09 20:26:45 +0500

    checkPoints grid

--- CheckDrive.Web/CheckDrive.Web/Controllers/CheckPointController.cs
+++ CheckDrive.Web/CheckDrive.Web/Controllers/CheckPointController.cs
@@ -3,16 +3,18 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace CheckDrive.Web.Controllers
 {
-    public class CheckPointController : Controller
+    public class CheckPointController(ICheckPointStore checkPointStore) : Controller
     {
-        private readonly ICheckPointStore _checkPointStore;
-        public CheckPointController(ICheckPointStore checkPointStore)
-        {
-            _checkPointStore = checkPointStore ?? throw new ArgumentNullException(nameof(checkPointStore));
-        }
         public async Task<ActionResult> Index(int id)
         {
-            var checkPoint = await _checkPointStore.GetCheckPointByIdAsync(id);
+            var checkPoint = await checkPointStore.GetByIdAsync(id);
+
+            return View(checkPoint);
+        }
+
+        public async Task<ActionResult> Details(int id)
+        {
+            var checkPoint = await checkPointStore.GetByIdAsync(id);
 
             return View(checkPoint);
         }
