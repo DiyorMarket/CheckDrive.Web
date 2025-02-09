625928d41e0cc2f83b7f9163a02d78b57c3ad99a
Boburmirzo <137428656+Boburm1rzo@users.noreply.github.com>
2025-02-09 20:27:31 +0500

    checkPoint popUp

--- CheckDrive.Web/CheckDrive.Web/ViewModels/CheckPoint/CheckPointViewModel.cs
+++ CheckDrive.Web/CheckDrive.Web/ViewModels/CheckPoint/CheckPointViewModel.cs
@@ -9,17 +9,25 @@ public class CheckPointViewModel
     public string Driver { get; set; }
     public string CarModel { get; set; }
     public decimal CurrentMileage { get; set; }
+    public string FormattedCurrentMileage => CurrentMileage.ToString("N1");
     public decimal CurrentFuelAmount { get; set; }
+    public string FormattedCurrentFuelAmount => CurrentFuelAmount.ToString("N1");
     public string Mechanic { get; set; }
     public decimal InitialMillage { get; set; }
+    public string FormattedInitialMillage => InitialMillage.ToString("N1");
     public decimal FinalMileage { get; set; }
+    public string FormattedFinalMileage => FinalMileage.ToString("N1");
     public string Operator { get; set; }
     public decimal InitialOilAmount { get; set; }
+    public string FormattedInitialOilAmount => InitialOilAmount.ToString("N1");
     public decimal OilRefillAmount { get; set; }
+    public string FormattedOilRefillAmount => OilRefillAmount.ToString("N1");
     public string Oil { get; set; }
     public string Dispatcher { get; set; }
     public decimal FuelConsumptionAdjustment { get; set; }
+    public string FormattedFuelConsumptionAdjustment => FuelConsumptionAdjustment.ToString("N1");
     public decimal DebtAmount { get; set; }
+    public string FormattedDebtAmount => DebtAmount.ToString("N1");
     public CheckPointStage Stage { get; set; }
     public CheckPointStatus Status { get; set; }
 }
